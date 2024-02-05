aee0175cecb80df25a4c4923927466b4d171ae36
joshua@doableservices.com <joshua@doableservices.com>
2024-02-05 16:25:07 +0800

    ToastContainer was imported

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,11 +4,25 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import axios from 'axios';
+import 'react-toastify/dist/ReactToastify.css';
 
-// axios.defaults.baseURL = ""
+axios.defaults.baseURL = "http://localhost:1337/api/"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
+  <ToastContainer
+    position="top-right"
+    autoClose={3000}
+    hideProgressBar={false}
+    newestOnTop={false}
+    closeOnClick
+    rtl={false}
+    pauseOnFocusLoss
+    draggable
+    pauseOnHover
+    theme="light"
+  />
     <App />
+    <ToastContainer/>
   </BrowserRouter>,
 )
