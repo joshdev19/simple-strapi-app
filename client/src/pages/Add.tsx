de1e8bb1c5ab23857987e764cbacbae57f19f062
joshua@doableservices.com <joshua@doableservices.com>
2024-02-05 16:16:42 +0800

    Made new both instances in Add & Update Components

--- client/src/pages/Add.tsx
+++ client/src/pages/Add.tsx
@@ -1,6 +1,50 @@
+import { useState } from "react"
+import { PropTypes } from "../types/types"
+import axios from "axios";
+import { toast } from "react-toastify";
+
 const Add = () => {
+
+  const [ values, setValues ] = useState<PropTypes>(
+    {
+      title: '',
+      description: ''
+    }
+  )
+
+  const controlledInputs = ( e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement> ) => {
+    
+    const name: string = e.target.name;
+    const value: string = e.target.value;
+
+    setValues( p => ( { ...p, [name]: value } ) )
+
+  }
+
+  const addHandler = async () => {
+
+    try {
+      
+      const request = await axios.post('');
+
+      const response = await request.data;
+
+      console.log(response)
+
+    } catch (error) {
+      
+      console.log(error)
+
+    }
+
+  }
+
   return (
-    <div>Add</div>
+    <form>
+      <input type="text" name="" onChange={ controlledInputs } placeholder="Title..." value={ values?.title } />
+      <textarea name="description" onChange={ controlledInputs } placeholder="Description..." value={ values?.description } ></textarea>
+      <button type="button" onClick={ addHandler }> ADD </button>
+    </form>
   )
 }
 
--- client/src/pages/Update.tsx
+++ client/src/pages/Update.tsx
@@ -1,6 +1,68 @@
+import { useEffect, useState } from "react"
+import { PropTypes } from "../types/types"
+import { useParams } from "react-router-dom"
+import axios from "axios";
+import { toast } from "react-toastify";
+
 const Update = () => {
+
+  const { id } = useParams();
+
+  const [ values, setValues ] = useState<PropTypes>(
+    {
+      title: '',
+      description: ''
+    }
+  )
+  
+  useEffect(() => {
+
+    const fetchData = async () => {
+
+      try {
+        
+        const request = await axios.put('');
+
+        const response = await request.data;
+
+
+      } catch (error) {
+        
+        console.log(error)
+
+      }
+
+    }
+
+    fetchData();
+
+  }, [ id, setValues])
+
+  const controlledInputs = ( e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement> ) => {
+    
+    const name: string = e.target.name;
+    const value: string = e.target.value;
+
+    setValues( p => ( { ...p, [name]: value } ) )
+
+  }
+
+  const updateHandler = async () => {
+
+    try {
+      
+    } catch (error) {
+      
+    }
+
+  }
+
   return (
-    <div>Update</div>
+    <form>
+      <input type="text" name="" onChange={ controlledInputs } placeholder="Title..." value={ values?.title } />
+      <textarea name="description" onChange={ controlledInputs } placeholder="Description..." value={ values?.description } ></textarea>
+      <button type="button" onClick={ updateHandler }> ADD </button>
+    </form>
   )
 }
 
