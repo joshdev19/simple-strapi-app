33e2e44c9327de1c06233843fcca180fab4aea54
joshua@doableservices.com <joshua@doableservices.com>
2024-02-05 15:45:11 +0800

    New Instances in card component

--- client/src/components/Card.tsx
+++ client/src/components/Card.tsx
@@ -1,7 +1,15 @@
-const Card = () => {
+import { Link } from "react-router-dom"
+import { PropTypes } from "../types/types"
+
+const Card = ( { id, title, description }: PropTypes ) => {
+  
   return (
-    <div>Card</div>
+    <Link to={`/update/${id}`}>
+      <p className="title"> { title } </p>
+      <p className="description"> { description } </p>
+    </Link>
   )
+
 }
 
 export default Card
