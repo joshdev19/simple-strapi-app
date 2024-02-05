2337b572f5a497f09f261570d149b9ca5c6c59e1
joshua@doableservices.com <joshua@doableservices.com>
2024-02-05 18:01:19 +0800

    Made new changes

--- client/src/components/Card.tsx
+++ client/src/components/Card.tsx
@@ -6,7 +6,7 @@ const Card = ( { id, attributes }: AttributeTypes ) => {
   return (
     <Link to={`/update/${id}`}>
       <p className="title"> { attributes?.title } </p>
-      <p className="description"> { attributes?.description } </p>
+      <p className="description"> { attributes?.description.substring(0, 23) } ... </p>
     </Link>
   )
 
