6082b3df725c2a25e3fd1c6bd0c192eb177cb6c2
joshua@doableservices.com <joshua@doableservices.com>
2024-02-05 16:47:39 +0800

    Made new changes

--- client/src/pages/Index.tsx
+++ client/src/pages/Index.tsx
@@ -1,13 +1,45 @@
+import { useEffect, useState } from "react"
 import Card from "../components/Card"
 import Footer from "../components/Footer"
 import Nav from "../components/Nav"
+import axios from "axios"
+import { DataTypes, PropTypes } from "../types/types"
 
 const Index = () => {
+
+  const [ todos, setTodos ] = useState<DataTypes<PropTypes>[]>();
+
+  useEffect(() => {
+
+    const fetchData = async () => {
+
+      try {
+        
+        const request = await axios.get('todos');
+
+        const response = await request.data;
+
+        setTodos( response?.data )
+
+      } catch (error) {
+        
+        console.log(error)
+
+      }
+
+    }
+
+    fetchData();
+
+  }, [ setTodos])
+
   return (
     <div className="wrapper">
       <Nav/>
       <div className="cards-wrapper">
-        <Card id="1" title="test title" description="some description"/>
+        {
+          todos && todos.map( t => <Card key={ t?.data.id } { ...t?.data } /> )
+        }
       </div>
       <Footer/>
     </div>
